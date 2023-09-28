58ba8bfb1fb33bacf1f4f89e8ee8a7e6a8766beb
mershark <mersharktakyi@gmail.com>
2023-09-28 16:53:37 +0000

    Add join table for visits

--- schema.sql
+++ schema.sql
@@ -29,3 +29,35 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+-- Add 'join table' for visits
+CREATE TABLE vets(
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(255),
+age INT,
+date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+species_id INT,
+vet_id INT,
+PRIMARY KEY(species_id, vet_id),
+CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id),
+CONSTRAINT fk_vets 
+FOREIGN KEY(vet_id)
+REFERENCES vets(id)
+);
+
+CREATE TABLE visits(
+animal_id INT,
+vet_id INT,
+date_of_visits DATE,
+CONSTRAINT fk_animals
+FOREIGN KEY(animal_id)
+REFERENCES animals(id),
+CONSTRAINT fk_vets
+FOREIGN KEY(vet_id)
+REFERENCES vets(id)
+);
